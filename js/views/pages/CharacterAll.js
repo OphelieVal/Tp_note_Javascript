6881e7b29f01506c1147b0c7d7fb73637caa9ce4
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-03-19 15:36:36 +0100

    modifications characterAll

--- js/views/pages/CharacterAll.js
+++ js/views/pages/CharacterAll.js
@@ -1,14 +1,19 @@
 // Listing des personnages du data.json
 import CharacterProvider from "../../services/CharacterProvider.js";
+import DetailsCharacter from "./DetailsCharacter.js";
 export default class CharacterAll {
     async render(){
         let characters = await CharacterProvider.fetchCharacters(20);
+        console.log(characters);
+
         let view = `
         <h2>Les personnages</h2>
-        <ul>
+        <ul id='character_liste'>
         ${characters.map(character=>
             `
-            <li>${character.title}</li>
+            <img src="${character.img}" alt="image du character">
+            <a href="/character/${character.id}">${character.name}</a>
+            <p>Niveau : ${character.niveau}</p>
             `
         ).join('\n')}
         </ul>
--- js/views/pages/DetailsCharacter.js
+++ js/views/pages/DetailsCharacter.js
@@ -1,1 +1,3 @@
 // Détail d'un personnage avec possibilité de notation et de mise en favoris
+export default class DetailsCharacter{
+}
