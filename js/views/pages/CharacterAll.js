f4fb5344291dc10f94cf2da583efa682eeb3658c
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-03-19 14:04:55 +0100

    début de characterAll.js

--- js/views/pages/CharacterAll.js
+++ js/views/pages/CharacterAll.js
@@ -1,1 +1,18 @@
 // Listing des personnages du data.json
+import CharacterProvider from "../../services/CharacterProvider.js";
+export default class CharacterAll {
+    async render(){
+        let characters = await CharacterProvider.fetchCharacters(20);
+        let view = `
+        <h2>Les personnages</h2>
+        <ul>
+        ${characters.map(character=>
+            `
+            <li>${character.title}</li>
+            `
+        ).join('\n')}
+        </ul>
+        `;
+        return view;
+    }
+}
